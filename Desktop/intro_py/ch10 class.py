1bb66f1b937682ff9ca1c62b36864cd5b5dc139f
sooox0809 <sooox89@gmail.com>
2023-01-20 17:25:39 +0900

    ch10 class inherit

--- Desktop/intro_py/ch10 class.py
+++ Desktop/intro_py/ch10 class.py
@@ -1,13 +1,22 @@
-#ch10 
-class Cat:
-    pass
-a_cat = Cat()
-another_cat = Cat()
-a_cat.age = 3
-a_cat.name = 'Mr.Fuzzybutton'
-a_cat.nemesis = another_cat
+#ch10
 
-# print(a_cat.nemesis.name)  ## AttributeError: 'Cat' object has no attribute 'name'
-a_cat.nemesis.name = 'Mr.Bigglesworth'  # another_cat 클래스에 name 속성 할당 !
-print(a_cat.nemesis.name)
+# class Cat():
+#     def __init__(self,name):
+#         self.name = name
+# furball = Cat('Grumpy')
+# print('Our latest addition: ', furball.name)
+
+class Car():
+    def exclaim(self):
+        print("I'm a Car!")
+class Yugo(Car):
+    def exclaim(self):
+        print("I'm a Yugo! Much like a Car, but more Yugo-ish.")
+# print(issubclass(Yugo,Car))  ## true
+
+give_me_a_car = Car()  # Car class에 give_me_a_car 객체 생성
+give_me_a_yugo = Yugo()
+
+give_me_a_car.exclaim() # 객체에서 exclaim
+give_me_a_yugo.exclaim()
 
