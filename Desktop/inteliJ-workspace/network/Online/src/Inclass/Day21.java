a8b30eee851e6cd7f9b806e01bc32c5025197078
sooox0809 <skycasey@inha.edu>
2023-02-14 10:20:40 +0900

    Day22

---
+++ Desktop/inteliJ-workspace/network/Online/src/Inclass/Day21.java
@@ -0,0 +1,31 @@
+package Inclass;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Day21 {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://www.inha.ac.kr");
+            URLConnection urlConnection = url.openConnection();
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(urlConnection.getInputStream()));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            br.close();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException ex) {
+            System.out.println("test");
+        }
+
+
+
+    }
+}
---
+++ Desktop/inteliJ-workspace/network/Online/src/Inclass/Day22.java
@@ -0,0 +1,40 @@
+package Inclass;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Date;
+
+public class Day22 {
+    public static void main(String[] args) {
+        System.out.println("멀티캐스트 타임 서버");
+        DatagramSocket serverSocket = null ;
+        try{
+            serverSocket = new DatagramSocket();
+            while (true) {
+                String dateText = new Date().toString();
+                byte[] buffer = new byte[256];
+                buffer = dateText.getBytes();
+
+                InetAddress group = InetAddress.getByName("224.0.0.117");
+                DatagramPacket packet;
+                packet = new DatagramPacket(buffer, buffer.length, group, 10000);
+                serverSocket.send(packet);
+                System.out.println("전송된 시간: " + dateText);
+
+                try {
+                    Thread.sleep(5000); // 5초 단위 딜레이
+                } catch (InterruptedException ex) {
+                    // Handle exception
+                }
+            }
+
+        } catch (SocketException ex) {
+            // Handle exception
+        } catch (IOException ex) {
+            // Handle exception
+        }
+    }
+}
