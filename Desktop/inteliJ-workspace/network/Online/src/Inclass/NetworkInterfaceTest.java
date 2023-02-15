20ab60bd4d744551fe772197544b4b7f92ac0b89
sooox0809 <skycasey@inha.edu>
2023-02-15 14:10:27 +0900

    day 23

--- Desktop/inteliJ-workspace/network/Online/src/Inclass/NetworkInterfaceTest.java
+++ Desktop/inteliJ-workspace/network/Online/src/Inclass/NetworkInterfaceTest.java
@@ -1,5 +1,6 @@
 package Inclass;
 
+import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Collections;
@@ -12,11 +13,24 @@ public class NetworkInterfaceTest {
             System.out.printf("이름      디스플레이 이름\n");
             for (NetworkInterface element : Collections.list(interfaceEnum)) {
                 System.out.printf("%-8s  %-32s\n", element.getName(), element.getDisplayName());
+                Enumeration<InetAddress> addresses = element.getInetAddresses();
+//                for (InetAddress inetAddress : Collections.list(addresses)) {
+//                    System.out.printf("    InetAddress:%s\n", inetAddress);
+//                }
+                Collections.
+                        list(addresses)
+                        .stream()
+                        .forEach(
+                                (inetAddress) -> {
+                                    System.out.printf(" IP 주소 : %s\n", inetAddress);
+                                }
+                        );
+
             }
+
         } catch (SocketException ex) {
             System.out.println();
         }
     }
+
 }
-
-
